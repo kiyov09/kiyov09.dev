f7513ff3feaac8279b3311be056026d11dcf4e09
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2022-04-26 08:55:28 -0500

    Update body text color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="relative min-h-screen w-screen overflow-x-hidden bg-primary-dark px-6 font-poppins text-sm tracking-wide text-blue-100/75 lg:px-16">
+      <body className="relative min-h-screen w-screen overflow-x-hidden bg-primary-dark px-6 font-poppins text-sm tracking-wide text-blue-100/80 lg:px-16">
         <div className="absolute inset-x-0 -z-10 h-screen w-screen transform bg-transparent bg-[url(/hero-background-pattern.svg)] bg-auto bg-repeat opacity-70"></div>
         <Main />
         <NextScript />
