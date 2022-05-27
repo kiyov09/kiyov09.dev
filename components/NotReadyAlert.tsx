799165b3a8db268e940bacde8e94eb7483df789b
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2022-05-27 18:06:14 -0500

    Add horizontal paddinf to the alert message

--- components/NotReadyAlert.tsx
+++ components/NotReadyAlert.tsx
@@ -1,6 +1,6 @@
 export default function NotReadyAlert() {
   return (
-    <div className="absolute inset-x-0 bg-red-500 py-2 text-center text-xs text-white">
+    <div className="absolute inset-x-0 bg-red-500 py-2 px-4 text-center text-xs text-white">
       This site is under construction and is not fully functional yet. Sorry for
       the inconvenience.
     </div>
