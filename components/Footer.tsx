d6e1e6c926273f80d76c8e26e612be6f48f2628b
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2022-05-27 19:02:30 -0500

    Fix footer cta position

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -71,10 +71,11 @@ function MessageBtn() {
   return (
     <button
       className="
-        border-1 group relative block border-transparent z-10
-        bg-clip-padding before:absolute before:inset-0 before:-z-10 before:-m-0.5 before:rounded-md
-        before:bg-blue-gradient after:absolute after:inset-0 after:-z-10 after:-m-0 after:rounded-md
-        after:bg-primary-dark hover:text-white hover:after:bg-blue-gradient"
+        border-1 group
+        relative z-10 mx-auto block border-transparent bg-clip-padding
+        before:absolute before:inset-0 before:-z-10 before:-m-0.5 before:rounded-md before:bg-blue-gradient
+        after:absolute after:inset-0 after:-z-10 after:-m-0 after:rounded-md after:bg-primary-dark
+        hover:text-white hover:after:bg-blue-gradient lg:mx-0"
     >
       <span className="inline-block rounded-lg bg-blue-gradient bg-clip-text py-3 px-5 font-medium text-transparent group-hover:text-white">
         Send me an email
