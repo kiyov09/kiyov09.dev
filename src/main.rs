01ab316b74e49b5fe4946d69c18b787460c9ad49
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-10-27 17:43:02 -0600

    feat: askama templates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use axum::response::Html;
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/blog", get(nothing_yet))
+        .route("/projects", get(nothing_yet))
+        .route("/contact", get(nothing_yet));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5001));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -13,6 +17,49 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> Html<&'static str> {
-    "Hello, World!".into()
+async fn root() -> templates::Home {
+    templates::Home {}
+}
+
+async fn nothing_yet() -> templates::NothingYet {
+    templates::NothingYet {}
+}
+
+pub mod models {
+    pub struct Link {
+        pub label: &'static str,
+        pub href: &'static str,
+    }
+}
+
+pub mod templates {
+    use crate::models;
+    use askama::Template;
+
+    pub const LINKS: [models::Link; 4] = [
+        models::Link {
+            label: "Home",
+            href: "/",
+        },
+        models::Link {
+            label: "Blog",
+            href: "/blog",
+        },
+        models::Link {
+            label: "Projects",
+            href: "/projects",
+        },
+        models::Link {
+            label: "Contact",
+            href: "/contact",
+        },
+    ];
+
+    #[derive(Template)]
+    #[template(path = "index.html")]
+    pub struct Home;
+
+    #[derive(Template)]
+    #[template(path = "nothing_yet.html")]
+    pub struct NothingYet;
 }
