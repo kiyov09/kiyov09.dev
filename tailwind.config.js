169a8dd81ea990c7d950e00f989be7deb7205b9a
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-11-17 09:57:32 -0600

    feat: switch to gruvbox colorscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Gruvbox dark colors
+        "gruvbox-bg": "#282828",
+        "gruvbox-fg": "#fbf1c7",
+        "gruvbox-red": "#fb4934",
+        "gruvbox-green": "#b8bb26",
+        "gruvbox-yellow": "#fabd2f",
+        "gruvbox-blue": "#83a598",
+        "gruvbox-purple": "#d3869b",
+        "gruvbox-aqua": "#8ec07c",
+        "gruvbox-gray": "#928374",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
